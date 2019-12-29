396798f7db4353ba25e7ea20c264f8d769b08111
Vittaly <vitaly.burkut@gmail.com>
2019-12-29 12:10:34 +0500

    git ignore

---
+++ firmWare/powerSupplyArduino/powerSupplyArduino/main.c
@@ -0,0 +1,196 @@
+/*
+ * powerSupplyArduino.c
+ *
+ * Created: 26.12.2019 19:34:45
+ * Author : Nataly
+ */ 
+
+#define F_CPU 8000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include <stdlib.h>
+#include "avr/interrupt.h"
+#include "string.h"
+
+
+#define CUR_LIMIT_ON PORTB |= 0x01
+#define CUR_LIMIT_OFF PORTB&=0xFE
+#define CUR_LIMIT_INV PORTB^=0x01
+#define BLINK_POST_SCAL 25
+uint8_t blink = 1;
+
+uint8_t blinkPostScal =  BLINK_POST_SCAL;
+
+uint16_t  settedVoltage = 50;
+uint16_t  settedCurrent = 20;
+uint16_t  CurrentCurrent = 10;
+uint16_t  CurrentVoltage = 2550;
+
+uint8_t mode = 0; // 0 - work mode (display cur volumes), 1 - set voltage, 2 - set current
+uint8_t need2refresh = 0;
+
+uint8_t lastEncoder;
+
+char lcdStr0[16+1];
+char lcdStr1[16+1];
+char bufStr[8+1];
+
+
+
+void readEncoder(void){
+     //mode = PORTB;
+     uint8_t curEncoder = PINB & (7<<5);
+      if (lastEncoder  == 0 || curEncoder == lastEncoder) {lastEncoder = curEncoder; return;}
+    // check press button
+     if (curEncoder & (1 << PINB7) && !(lastEncoder & (1 << PINB7) )) mode = (mode+1) % 3;
+    
+     // to left
+     if ((curEncoder==128 && lastEncoder == 192 )|| (curEncoder == 160 && lastEncoder == 128)) {
+switch (mode) { case 1: settedVoltage-=10; break; case 2: settedCurrent-=10; break;}
+
+}
+    // to right
+    if ((curEncoder == 128 && lastEncoder == 160) || (curEncoder == 192 && lastEncoder == 128)) {
+         switch (mode) { case 1: settedVoltage+=10; break; case 2: settedCurrent+=10; break;}
+        
+
+}
+
+
+    lastEncoder = curEncoder;
+}
+
+//interrupts
+ISR(TIMER0_OVF_vect){
+    blinkPostScal--;
+    readEncoder();
+    if (!blinkPostScal){
+        CUR_LIMIT_INV;
+        blink=~blink;
+        blinkPostScal = BLINK_POST_SCAL;
+        //displeyCurVal();
+      ++need2refresh;
+      /*  if (blink){
+        lcd_return_home();
+        lcd_puts("50");
+        } else
+        {
+          lcd_return_home();
+          lcd_puts("  ");  
+        }*/
+}
+}
+
+void displeyVal(void){
+    lcd_return_home();
+
+    itoa(mode==1? settedVoltage:CurrentVoltage, bufStr, 10);
+
+ // display voltage in first line
+    for (int i = 0 ; i < LCD_COL_COUNT; i++) lcdStr0[i] = ' ';
+   char len;
+    if (mode != 1 || blink==1 )
+    {
+    
+    lcdStr0[0] = 'V';
+    lcdStr0[1] = ':';
+    
+    len = strlen(bufStr);
+    switch (len) {
+    case 4: lcdStr0[2] = bufStr[0]; lcdStr0[3] = bufStr[1]; lcdStr0[4] = ','; lcdStr0[5] = bufStr[2]; lcdStr0[6] = bufStr[3]; break;
+    case 3: lcdStr0[2] = bufStr[0]; lcdStr0[3] = ','; lcdStr0[4] = bufStr[1]; lcdStr0[5] = bufStr[2];  break;
+    case 2: lcdStr0[2] = '0'; lcdStr0[3] = ','; lcdStr0[4] = bufStr[0]; lcdStr0[5] = bufStr[1]; break;
+}
+
+    lcdStr0[len==4?7:6] = ' ';
+    lcdStr0[len==4?8:7] = 'B';
+}
+  // display current in second line
+    itoa(mode==2 ? settedCurrent: CurrentCurrent, bufStr, 10);
+    len = strlen(bufStr);
+    for (int i = 0 ; i < LCD_COL_COUNT; i++) lcdStr1[i] = ' ';
+    if ( mode != 2  || blink == 1) {
+    lcdStr1[0] = 'C';
+    lcdStr1[1] = ':';     
+
+    switch (strlen(bufStr)) {
+        case 4: lcdStr1[2] = bufStr[0]; lcdStr1[3] = bufStr[1]; lcdStr1[4] = ','; lcdStr1[5] = bufStr[2]; lcdStr1[6] = bufStr[3];  break;
+        case 3: lcdStr1[2] = bufStr[0]; lcdStr1[3] = ','; lcdStr1[4] = bufStr[1]; lcdStr1[5] = bufStr[2];  break;
+        case 2: lcdStr1[2] = '0'; lcdStr1[3] = ','; lcdStr1[4] = bufStr[0]; lcdStr1[5] = bufStr[1];  break;
+    }
+    lcdStr1[len==4?7:6] = ' ';
+    lcdStr1[len==4?8:7] = 'A';
+}
+    lcd_puts(lcdStr0);
+    lcd_set_cursor(0,1);
+    lcd_puts(lcdStr1);
+
+  //debug info
+  lcd_set_cursor(10,1);
+  itoa(lastEncoder, bufStr, 10);
+  lcd_puts(bufStr);
+
+}
+
+void initPorts(void){
+// current limit led out mode
+    DDRB  = 0x01;
+// encoder pins as input
+    DDRB &= ~(7<<5);
+    PORTB|= (7<<5); //pull up
+//ADT
+//http://narodstream.ru/avr-urok-22-izuchaem-acp-chast-2/
+ADCSRA |= (1<<ADEN) // Разрешение использования АЦП
+|(1<<ADPS2)|(1<<ADPS1);//Делитель 64 = 128 кГц  
+ADMUX |= (1<<REFS1)|(1<<REFS0); //Внутренний Источник ОН 2,56в, вход ADC0
+
+}
+
+void initTimers(void){
+TIMSK =(1<<TOIE0);  // timer0 enable
+TCCR0 = (1<<CS02); // prescaler 1/256
+
+}
+
+
+
+int main(void)
+{
+	
+	initPorts();
+    initTimers();
+    
+    lcd_clear();
+    lcd_init();
+    
+    lcd_on();
+   // lcd_set_cursor(0,0);
+    //lcd_enable_blinking();
+  //  lcd_puts("Set:");
+   // itoa(settedVoltage,str,10);
+  //  lcd_return_home();
+ //   lcd_set_cursor(1,1);
+  //  lcd_puts(str);
+    sei();
+    /* Replace with your application code */
+   // displeyCurVal();
+    while (1) 
+    {
+        if (need2refresh) {need2refresh = 0; displeyVal();}
+displeyVal();
+ //   	CUR_LIMIT_INV;
+    /*    for (char i=0; i < 2; i++) {
+            lcd_set_cursor(0,i);
+            lcd_puts("     ");
+        }
+		_delay_ms(300);
+        for (char i=0; i < 2; i++) {
+            lcd_set_cursor(0,i);
+            lcd_puts("50    ");
+        }
+		_delay_ms(300);*/
+    }
+}
+
