8cd7de588800dd24a9743dd42c277585fa1cdee1
Vittaly <vitaly.burkut@gmail.com>
2019-12-29 23:26:13 +0500

    refresh display onli if it is needed

--- firmWare/powerSupplyArduino/powerSupplyArduino/main.c
+++ firmWare/powerSupplyArduino/powerSupplyArduino/main.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include "avr/interrupt.h"
 #include "string.h"
+#include "avr/eeprom.h"
 
 enum modes {NORMAL_MODE, VOLTAGE_SET_MODE, CURRENT_SET_MODE} mode; // 0 - work mode (display cur volumes), 1 - set voltage, 2 - set current
 
@@ -33,12 +34,15 @@ uint8_t blink = 1;
 uint8_t blinkPostScal =  BLINK_POST_SCAL;
 
 uint16_t  settedVoltage = 50;
+uint16_t  settedVoltageEeprom EEMEM = 100;
 uint16_t  settedCurrent = 20;
+uint16_t  settedCurrentEeprom EEMEM = 100 ;
 uint16_t  CurrentCurrent = 10;
 uint16_t  CurrentVoltage = 2550;
+//uint8_t test = 0;
 
 //uint8_t mode = 0; // 0 - work mode (display cur volumes), 1 - set voltage, 2 - set current
-uint8_t need2refresh = 0;
+uint8_t LcdNeed2refresh = 0;
 
 uint8_t lastEncoder;
 
@@ -52,7 +56,14 @@ void readEncoder(void){
     uint8_t curEncoder = PINB & (7<<5);
     if (lastEncoder  == 0 || curEncoder == lastEncoder) {lastEncoder = curEncoder; return;}
     // check press button
-    if (curEncoder & (1 << PINB7) && !(lastEncoder & (1 << PINB7) )) mode = (mode+1) % 3;
+    if (curEncoder & (1 << PINB7) && !(lastEncoder & (1 << PINB7) )) {
+        mode = (mode+1) % 3;
+        switch (mode) { 
+            case NORMAL_MODE: eeprom_write_word(&settedCurrentEeprom, settedCurrent);
+            case CURRENT_SET_MODE: eeprom_write_word(&settedVoltageEeprom, settedVoltage);
+            default: break;
+            }
+    }
     
     // to left
     if ((curEncoder==128 && lastEncoder == 192 )|| (curEncoder == 160 && lastEncoder == 128)) {
@@ -61,7 +72,7 @@ void readEncoder(void){
             if (settedVoltage>=ENCODER_STEP) settedVoltage-=10;  break;
             case CURRENT_SET_MODE:
             if (settedCurrent>=ENCODER_STEP) settedCurrent-=10; break;
-            case NORMAL_MODE: break;
+            default: break;
         }
 
     }
@@ -72,7 +83,7 @@ void readEncoder(void){
             if (settedVoltage < VOLTAGE_MAX - ENCODER_STEP) settedVoltage+=10; break;
             case CURRENT_SET_MODE:
             if (settedCurrent < CUR_MAX - ENCODER_STEP)  settedCurrent+=10; break;
-            case NORMAL_MODE: break;
+            default: break;
         }
         
 
@@ -80,6 +91,9 @@ void readEncoder(void){
     // set value to PWM
     OCR1B = VAL2PWM10(settedVoltage, VOLTAGE_MAX);
     OCR1A = VAL2PWM10(settedCurrent , CUR_MAX);
+    // fix that display should be refreshed
+    LcdNeed2refresh = 1;
+    
 
     lastEncoder = curEncoder;
 }
@@ -93,7 +107,7 @@ ISR(TIMER0_OVF_vect){
         blink=~blink;
         blinkPostScal = BLINK_POST_SCAL;
         //displeyCurVal();
-        ++need2refresh;
+        ++LcdNeed2refresh;
         /*  if (blink){
         lcd_return_home();
         lcd_puts("50");
@@ -106,6 +120,7 @@ ISR(TIMER0_OVF_vect){
 }
 
 void displeyVal(void){
+    if (LcdNeed2refresh == 0) {return;} else {LcdNeed2refresh = 0;};
     lcd_return_home();
 
     itoa(mode==1? settedVoltage:CurrentVoltage, bufStr, 10);
@@ -123,7 +138,9 @@ void displeyVal(void){
         switch (len) {
             case 4: lcdStr0[2] = bufStr[0]; lcdStr0[3] = bufStr[1]; lcdStr0[4] = ','; lcdStr0[5] = bufStr[2]; lcdStr0[6] = bufStr[3]; break;
             case 3: lcdStr0[2] = bufStr[0]; lcdStr0[3] = ','; lcdStr0[4] = bufStr[1]; lcdStr0[5] = bufStr[2];  break;
+            case 1:
             case 2: lcdStr0[2] = '0'; lcdStr0[3] = ','; lcdStr0[4] = bufStr[0]; lcdStr0[5] = bufStr[1]; break;
+            
         }
 
         lcdStr0[len==4?7:6] = ' ';
@@ -140,6 +157,7 @@ void displeyVal(void){
         switch (strlen(bufStr)) {
             case 4: lcdStr1[2] = bufStr[0]; lcdStr1[3] = bufStr[1]; lcdStr1[4] = ','; lcdStr1[5] = bufStr[2]; lcdStr1[6] = bufStr[3];  break;
             case 3: lcdStr1[2] = bufStr[0]; lcdStr1[3] = ','; lcdStr1[4] = bufStr[1]; lcdStr1[5] = bufStr[2];  break;
+            case 1:
             case 2: lcdStr1[2] = '0'; lcdStr1[3] = ','; lcdStr1[4] = bufStr[0]; lcdStr1[5] = bufStr[1];  break;
         }
         lcdStr1[len==4?7:6] = ' ';
@@ -150,9 +168,18 @@ void displeyVal(void){
     lcd_puts(lcdStr1);
 
     //debug info
+    
+     lcd_set_cursor(12,0);
+     itoa(110, bufStr, 10);
+     lcd_puts(bufStr);
+    
     lcd_set_cursor(10,1);
     itoa(lastEncoder, bufStr, 10);
     lcd_puts(bufStr);
+    
+   
+    
+    
 
 }
 
@@ -174,8 +201,8 @@ void initTimers(void){
     TIMSK =(1<<TOIE0);  // timer0 OF interrupt enable
     TCCR0 = (1<<CS02); // prescaler for timer 0  = 1/256
     // timer 1 pwm for 2 channel
-    TCCR1A = 0b10100011; //COM1x1 + COM1x2 = 10b (non invert fast pwm) FOC1A = 0, FOC1B = 0, WGM11 = 1, WGM10 = 1
-    TCCR1B = 0b00001001; //ICNC1 =0, ICES1 =0, -, WGM13 = 0, WGM12 = 1, SC12 = 0, SC11=0, SC10 = 1 // 10 bit fast PWM , no prescaling
+  //  TCCR1A = 0b10100011; //COM1x1 + COM1x2 = 10b (non invert fast pwm) FOC1A = 0, FOC1B = 0, WGM11 = 1, WGM10 = 1
+ //   TCCR1B = 0b00001001; //ICNC1 =0, ICES1 =0, -, WGM13 = 0, WGM12 = 1, SC12 = 0, SC11=0, SC10 = 1 // 10 bit fast PWM , no prescaling
 
 }
 
@@ -198,12 +225,17 @@ int main(void)
     //  lcd_return_home();
     //   lcd_set_cursor(1,1);
     //  lcd_puts(str);
-    sei();
-    /* Replace with your application code */
-    // displeyCurVal();
+    settedVoltage =  eeprom_read_word(&settedVoltageEeprom);
+    settedCurrent = eeprom_read_word(&settedCurrentEeprom);
+    sei();    
+    
+     displeyVal();
+     
     while (1)
     {
-        if (need2refresh) {need2refresh = 0; displeyVal();}
+       // test++;
+      //  if (LcdNeed2refresh > 0) {
+      //    displeyVal(0);  displeyVal(1); LcdNeed2refresh = 0;}
         displeyVal();
         //   	CUR_LIMIT_INV;
         /*    for (char i=0; i < 2; i++) {
